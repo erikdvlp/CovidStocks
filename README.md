3e7c65bec6d16d9bf42018fc0ce064f13283410b
Erik Taylor <erikdvlp@gmail.com>
2020-03-18 15:40:46 -0400

    Documentation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CovidStocks
  
+Scrapes stock index values to calculate how much the markets have fallen since the global COVID-19 health emergency.
--- scrape.py
+++ scrape.py
@@ -1,19 +1,19 @@
 from bs4 import BeautifulSoup
-import requests
+import requests as r
 import commodity as c
 
 def scrape():
-	#DOW Jones
+	#DOW
 	dow = c.Commodity("DOW", "https://money.cnn.com/data/markets/dow/", 29568.57)
-	page = BeautifulSoup(requests.get(dow.src).content, 'html.parser')
+	page = BeautifulSoup(r.get(dow.src).content, 'html.parser')
 	raw = page.find("td", {"class": "wsod_last wsod_lastIndex"}).findAll("span")
 	dow.price = float(raw[0].text.replace(',',''))
-	dow.time = raw[1].text
+	dow.time = raw[1].text[:-3]
 	dow.calcDiff()
 
 	#TSX
 	tsx = c.Commodity("TSX", "https://www.theglobeandmail.com/investing/markets/indices/", 17970.50)
-	page = BeautifulSoup(requests.get(tsx.src).content, 'html.parser')
+	page = BeautifulSoup(r.get(tsx.src).content, 'html.parser')
 	raw = page.find("span", {"class": "barchart-overview-field-value"}).find("barchart-field").attrs['value']
 	tsx.price = float(raw.replace(',',''))
 	tsx.calcDiff()
